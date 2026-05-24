1c76d23e10866d867c1f0a250a6ec2bd2fd4c5be
Andrés Martin Mena <andres.martin.m@ug.uchile.cl>
2026-05-24 07:37:13 -0400

    latex

---
+++ sketch.js
@@ -0,0 +1,64 @@
+// declarar variables de recursos
+
+// textos
+
+// imagenes
+
+// colores
+
+// tipografias
+
+
+
+function setup() 
+{
+    // cargar recursos png jpg svg json
+    
+	createCanvas(600, 800);
+    background(220);
+    noStroke();
+
+    // dibujar fondo
+    imagenFondo();
+
+    // dibujar cuadro para texto
+    cuadroTexto();
+
+    // dibujar texto
+    texto();
+
+    // dibujar efectos
+    efectos(0);
+
+    // dibujar logos
+    logos();
+}
+
+function draw()
+{
+    
+}
+
+// imagen de fondo
+
+function imagenFondo(){
+
+}
+
+function cuadroTexto(){
+
+}
+
+function texto(){
+
+}
+
+function efectos(seed){
+    for (let i = 0; i < 15; i++) {
+        
+    }
+}
+
+function logos(){
+
+}
